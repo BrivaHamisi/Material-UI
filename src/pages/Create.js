1acac77e2b106da0ab4135920da43e531dc88785
Hamisi-Esq <brivahamisi@gmail.com>
2022-09-12 00:43:15 +0300

    Button and Button group

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { ButtonGroup } from '@mui/material';
 
 
 export default function Create() {
@@ -8,6 +10,17 @@ export default function Create() {
       <Typography variant='h6' color ='textSecondary' component='h2' gutterBottom>
         Create a new Note
       </Typography>
+      <Button  type="submit" color="primary">
+        SUBMIT
+      </Button>
+      <Button  type="submit" color="secondary" variant="outlined">
+        SUBMIT
+      </Button>
+      <ButtonGroup color='secondary' variant='contained'>
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
     </div>
   )
 }
