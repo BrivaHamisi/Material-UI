1d6c7ee33a89450187ee4e0511f5d1c29e678635
Hamisi-Esq <brivahamisi@gmail.com>
2022-09-13 11:32:26 +0300

    TextField and Validation MUI

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -3,22 +3,71 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { Container } from '@mui/material';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import TextField from '@mui/material/TextField';
+import { useState } from "react";
+
+import { styled } from '@mui/material/styles'
+
+const MyField = styled(TextField)({
+  marginTop: 20,
+  marginBottom: 20,
+  display: 'block',
+})
 
 
 export default function Create() {
+
+const [title, setTitle] = useState("")
+const [details, setDetails] = useState("")
+
+const [titleError, setTitleError] = useState(false)
+const [detailsError, setDetailsError] = useState(false)
+
+const handleSubmit = (e) =>{
+  e.preventDefault()
+
+  setTitleError(false)
+  setDetailsError(false)
+
+  if (title ===''){
+    setTitleError(true)
+  }
+
+  if (details === ''){
+    setDetailsError(true)
+  }
+
+  if (title && details){
+    console.log(title, details)
+  }
+}
+
   return (
     <Container>
       <Typography variant='h6' color ='textSecondary' component='h2' gutterBottom>
         Create a new Note
       </Typography>
 
-      <Button
+      <form noValidate autoComplete='off' onSubmit={handleSubmit}>
+        <MyField 
+        onChange={(e)=> setTitle(e.target.value)}
+        label='Note Title' variant='outlined' color='secondary' fullWidth required false error={titleError}
+        />
+
+      <MyField 
+      onChange={(e)=> setDetails(e.target.value)}
+        label='Details' variant='outlined' color='secondary' fullWidth required multiline rows={5} error={detailsError}
+        />
+        <Button
       onClick={
         () => console.log('You Clicked Me')
       }
        type='submit'color='secondary' variant='contained' endIcon={<KeyboardArrowRightIcon/>}>
         Submit
       </Button>
+      </form>
+
+      
 
       {/* Icons
       <br/>
