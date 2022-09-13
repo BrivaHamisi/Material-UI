a5d6ef211b3f8d4c34076127ed883119ba1c38c8
Hamisi-Esq <brivahamisi@gmail.com>
2022-09-14 01:00:56 +0300

    Card Components ad handle Delete

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import { Container } from '@mui/system';
+import NoteCard from '../components/NoteCard';
 
 export default function Notes() {
 
@@ -15,32 +15,23 @@ export default function Notes() {
     .then(data => setNotes(data))
   }, [])
 
+  const handleDelete = async (id) => {
+    await fetch('http://localhost:8000/notes/' + id, {
+      method: 'DELETE'
+    })
+    const newNotes = notes.filter(note => note.id !== id)
+    setNotes(newNotes)
+  }
+
   return (
     <Container>
       <Grid container spacing={3}>
         {notes.map(note => (
           <Grid item xs={12} md={6} lg={4} key={note.id}>
-            <Paper>{note.title}</Paper>
+            <NoteCard note={note} handleDelete={handleDelete}/>
           </Grid>
         ))}
       </Grid>
     </Container>
   )
 }
-
-
-
-{/* <Grid container spacing={3}>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>1</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>2</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>3</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>4</Paper>
-        </Grid>
-      </Grid> */}
