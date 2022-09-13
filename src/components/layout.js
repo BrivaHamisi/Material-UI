cc7be5f0d263c36fde3b6650c4c552959e94578f
Hamisi-Esq <brivahamisi@gmail.com>
2022-09-14 02:15:09 +0300

    Permanent Drawer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,43 @@
 import React from 'react'
 import { makeStyles } from '@mui/styles';
+import Drawer from '@mui/material/Drawer';
+import Typography from '@mui/material/Typography'
+
+const drawerWidth = 240
 
 const useStyles = makeStyles({
   page: {
     background: '#f9f9f9',
     width: '100%'
-  }
+  },
+  root: {
+    display: 'flex',
+  },
+  drawer:{
+    
+    variant: 'permanent',
+    anchor: 'left'
+  },
 })
 
 export default function Layout({children}) {
     const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
         {/* app bar */}
       <div>app bar</div>
       
       {/* side drawer */}
-      <div>side drawer</div>
+      <Drawer sx ={{
+        width: drawerWidth,".MuiDrawer-paper": {width: drawerWidth},
+      }} variant="permanent"
+        anchor="left">
+        <div>
+            <Typography variant='h5'>
+                Write Notes
+            </Typography>
+        </div>
+      </Drawer >
 
       {/* main content */}
         <div className={classes.page}>
