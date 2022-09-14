3ece18304e84e9ab5928e2a3e4344eef09b348e4
Hamisi-Esq <brivahamisi@gmail.com>
2022-09-14 11:50:55 +0300

    List and List Items

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,14 @@ import React from 'react'
 import { makeStyles } from '@mui/styles';
 import Drawer from '@mui/material/Drawer';
 import Typography from '@mui/material/Typography'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import { AddCircleOutlineOutlined, SubjectOutlined } from '@mui/icons-material';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const drawerWidth = 240
 
@@ -18,10 +26,29 @@ const useStyles = makeStyles({
     variant: 'permanent',
     anchor: 'left'
   },
+  active: {
+    background: '#f4f4f4'
+  }
 })
 
 export default function Layout({children}) {
     const classes = useStyles();
+
+    const history = useHistory()
+    const location = useLocation()
+
+    const menuItems=[
+        {
+            text: 'My Notes',
+            icon: <SubjectOutlined color='secondary'/>,
+            path: '/'
+        },
+        {
+            text: 'Create Note',
+            icon: <AddCircleOutlineOutlined color='secondary'/>,
+            path: '/create'
+        }
+    ]
   return (
     <div className={classes.root}>
         {/* app bar */}
@@ -37,6 +64,17 @@ export default function Layout({children}) {
                 Write Notes
             </Typography>
         </div>
+        {/* List - Links */}
+        <List>
+            {menuItems.map(item =>(
+                <ListItem className={location.pathname === item.path ? classes.active : null } button key = {item.text} onClick={()=> history.push(item.path)}>
+                    <ListItemIcon>
+                        {item.icon}
+                    </ListItemIcon>
+                    <ListItemText primary={item.text}/>
+                </ListItem>
+            ))}
+        </List>
       </Drawer >
 
       {/* main content */}
